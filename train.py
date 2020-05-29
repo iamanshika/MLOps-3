a1e24e668a85a752c45dd5a192c3b35db4ca177e
iamanshika <64534699+iamanshika@users.noreply.github.com>
2020-05-29 10:10:03 +0530

    Optimized

--- train.py
+++ train.py
@@ -19,7 +19,7 @@ model.compile(optimizer='adam',
 
 model.fit(train_images,
            train_labels,
-           epochs=10,
+           epochs=5,
            validation_data = (test_images, test_labels))
 
 
